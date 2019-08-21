52f3ac8378a41bc84e861fccac30e31d5f737371
Broken-Admin <brokengaming50@gmail.com>
2019-08-20 20:52:25 -0400

    Replace instances of "url" with "info"

--- server.py
+++ server.py
@@ -13,11 +13,11 @@ while True:
   while True:
 
     try:
-      url_file = open("sent-info.txt", 'a')
+      info_file = open("sent-info.txt", 'a')
       data = conn.recv(1024)
       if not data: break
-      url_file.write(str(data)+'\n')
-      url_file.close()
+      info_file.write(str(data)+'\n')
+      info_file.close()
 
     except socket.error:
       print "Connection to client lost."
